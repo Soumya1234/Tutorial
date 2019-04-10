7c3619456a6902b4fe41f23cd1c9baea75622d41
Soumyadeep <secondorbit18@gmail.com>
2019-04-10 22:37:56 +0530

    Added TryWithResourcesDemo.java from downloaded from Udemy Course

--- src/com/udemy/exceptions/ExceptionDemo2.java
+++ src/com/udemy/exceptions/ExceptionDemo2.java
@@ -3,12 +3,14 @@ package com.udemy.exceptions;
 import java.io.IOException;
 
 public class ExceptionDemo2 {
+	@SuppressWarnings("unused")
 	private static void fileCopyWithARM() throws IOException {
 		try (Test t = new Test(); Test2 t2 = new Test2()) {
 			throw new IOException("Important Exception");
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private static void fileCopyWithoutARM() throws IOException {
 		Test t = null;
 		Test2 t2 = null;
@@ -31,14 +33,18 @@ public class ExceptionDemo2 {
 		}
 	}
 
-	private static void fileCopy2() {
+	private static void fileCopy2() throws IOException {
 		Test t = null;
 		Test2 t2 = null;
+		int i = 1;
 		IOException ioException = null;
 		try {
 			t = new Test();
 			t2 = new Test2();
-			throw new IOException("Important Exception");
+			if (i == 1) {
+				throw new IOException("Important Exception");
+			}
+
 		} catch (IOException e) {
 			ioException = e;
 		} finally {
@@ -48,36 +54,38 @@ public class ExceptionDemo2 {
 				} catch (IOException e) {
 					ioException.addSuppressed(e);
 				}
-
-				try {
-					t.close();
-				} catch (IOException e) {
-					ioException.addSuppressed(e);
-
-				}
 			} else {
 				try {
 					t2.close();
 				} catch (IOException e) {
 					ioException = e;
 				}
-
+			}
+			if (ioException != null) {
 				try {
 					t.close();
 				} catch (IOException e) {
 					ioException.addSuppressed(e);
+				}
+				throw ioException;
+			} else {
+				try {
+					t.close();
+				} catch (IOException e) {
+					e.printStackTrace();;
 
 				}
 
 			}
+			
 		}
 	}
 
 	public static void main(String[] args) {
 		try {
-			fileCopyWithoutARM();
+			fileCopy2();
 		} catch (IOException e) {
-			System.out.println("Exception thrown:" + e.getMessage());
+			e.printStackTrace();
 //			Throwable throwables[] = e.getSuppressed();
 //			System.out.println(throwables[0].getMessage());
 //			System.out.println(throwables[1].getMessage());
